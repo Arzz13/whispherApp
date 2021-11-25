c23e93b3921daf7e03cb59221bd9e4dd4a550c0b
Arshad <dragonarshad@gmail.com>
2021-11-25 18:41:48 +0530

    Level-4-Salted-Hash

--- app.js
+++ app.js
@@ -1,78 +1,89 @@
+require('dotenv').config(); //No need to assign to constant.This package will automatically load env variables from env file into the process in which this package is actively running.
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-require('dotenv').config();  //No need to assign to constant.This package will automatically load env variables from env file into the process in which this package is actively running.
-const express=require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
-const mongoose=require('mongoose');
-const md5=require("md5");
-
-const app=express();
-
+const app = express();
 
 
 app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema=new mongoose.Schema({
-    email:String,
-    password:String
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
 });
 
 
 
-const User=new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register",function(req,res){
-    
+app.get("/register", function (req, res) {
+
     res.render("register");
 });
 
 //When user enters data and click register 
-app.post("/register",function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-        password:md5(req.body.password)    //Saving the hash of password
+app.post("/register", function (req, res) {
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) { //generates a salt and hashes our password along with the salt "saltRound" number of times.The hashed password is returned to hash variable in call back function.
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash //Saving the hash of password
+        });
+        newUser.save(function (err) {
+            if (!err)
+                res.render("secrets");
+            else
+                console.log(err);
+        });
     });
-    newUser.save(function(err){
-        if(!err)
-        res.render("secrets");
-        else
-        console.log(err);
-    });
+
+
 });
 
-app.post("/login",function(req,res){
-    const userName=req.body.username;
-    const password=md5(req.body.password);  //We compare the hash of password that the user entered with the hash present in database.
+app.post("/login", function (req, res) {
+    const userName = req.body.username;
+    const password = req.body.password; //We compare the hash of password that the user entered with the hash present in database.
 
-    User.findOne({email:userName},function(err,foundUser){
-        if(err)
-        console.log(err);
+    User.findOne({
+        email: userName
+    }, function (err, foundUser) {
+        if (err)
+            console.log(err);
         else //No error
         {
-            if(foundUser) { //We found the user in the database with entered username.
-                if(foundUser.password===password){  //If password entered is correct.
-                    res.render("secrets");
-                }
-        
+            if (foundUser) { //We found the user in the database with entered username.
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                                                                         //We are comparing if the bcrypted version of password entered by user is equal to password in data base.If equal then result var is true else false 
+                    if (result === true)
+                        res.render("secrets");
+                });
             }
-        }
 
+        }
     });
+
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server started");
 });
